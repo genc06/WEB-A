1cae9a598c013b40e5dbde3ce7620be72fd2ee6f
Genc Demi <139209233+genc06@users.noreply.github.com>
2023-11-29 15:07:37 +0100

    ne pas utiliser root

--- NinaCreation/conn.php
+++ NinaCreation/conn.php
@@ -3,7 +3,7 @@ function conn()
 {
     try {
 
-        $connexion = new PDO("mysql:host=localhost;dbname=ninamodelelogique","root","");
+        $connexion = new PDO("mysql:host=localhost;dbname=ninamodelelogique","Admin_Nina","");
         $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $connexion;
